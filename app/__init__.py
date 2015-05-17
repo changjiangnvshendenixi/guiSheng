fadfd345c5d5fb5e7dfe1b58d563a1ea7b8c3ea9
朱承浩 <neo1218@users.noreply.github.com>
2015-05-17 10:51:41 +0800

    Update __init__.py
    1.添加文本信息注释
    2.修改缩进

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,19 @@
 # -*- coding: UTF-8 -*-
 #!/usr/bin/python
+
+# __init__.py
+# app包初始化文件
+# 1.app包导入其他文件
+# 2.创建蓝图app对象
+
 import os
 import re
 import json
 import random
 import urllib
 import datetime
-#导入模块和包
+
+# 导入模块和包
 from flask import Flask ,render_template,request,url_for,make_response
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -15,7 +22,8 @@ from flask.ext.bootstrap import Bootstrap
 from flask.ext.mail import Mail
 from flask.ext.pagedown import PageDown
 from config import config
-#初始化
+
+# 初始化
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
@@ -23,12 +31,14 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-#文件上传函数
+
+# 文件上传函数
 def gen_rnd_filename():
 	#以文件上传的日期确定文件名
 	filename_prefix = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 	return "%s%s" % (filename_prefix,str(random,randrange(1000,10000)))
-#创建工厂函数(实例化app)
+
+# 创建工厂函数(实例化app)
 def create_app(config_name):
 	#实例化Flask类创建app对象
 	app = Flask(__name__)
@@ -41,16 +51,15 @@ def create_app(config_name):
 	moment.init_app(app)
 	db.init_app(app)
 	login_manager.init_app(app)
+	# 蓝图注册
+    	from .main import main as main_blueprint
+    	app.register_blueprint(main_blueprint)
 
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-	#附加路由和自定义错误界面
-
-    from .auth import auth as auth_blueprint
+    	from .auth import auth as auth_blueprint
 	app.register_blueprint(auth_blueprint,url_prefix='/auth')
 
-    from .api_1_0 import api as api_1_0_blueprint
-    app.register_blueprint(api_1_0_blueprint,url_prefix='/api/v1.0')
+    	from .api_1_0 import api as api_1_0_blueprint
+    	app.register_blueprint(api_1_0_blueprint,url_prefix='/api/v1.0')
 
-    return app
+    	return app
 
